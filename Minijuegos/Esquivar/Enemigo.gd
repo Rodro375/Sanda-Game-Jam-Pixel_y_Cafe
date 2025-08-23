c29dc64b98dda8b724369c7dc554f6fa2cea69f4
Javier <jdac.javier@gmail.com>
2025-08-23 01:49:57 -0400

    TresComportamientos

--- Minijuegos/Esquivar/Enemigo.gd
+++ Minijuegos/Esquivar/Enemigo.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 enum Comportamiento{rafaga,circulo,especial}
-@onready var jugador = $"../Escenario/CharacterBody2D"
+@onready var jugador = $"../jugador"
 @export var comportamiento:Comportamiento = Comportamiento.rafaga
 @export var dificultad:int
 @export var intervalosDisparo: Array[float]=[0,0,0,0,0]
@@ -9,19 +9,22 @@ enum Comportamiento{rafaga,circulo,especial}
 @export var proyectilEspecial: PackedScene
 @export var espacioBalistico: Node2D
 func _physics_process(_delta: float) -> void:
-	look_at(jugador.position)
+	if comportamiento != Comportamiento.especial:
+		look_at(jugador.position)
 func _ready() -> void:
 	var relojDisparo:Timer= Timer.new()
 	relojDisparo.one_shot=false
 	add_child(relojDisparo)
-	relojDisparo.start(elegirIntervaloDisparo())
-	
-	relojDisparo.timeout.connect(func():elegirComportamientoDisparo())
-	
+	if comportamiento!=Comportamiento.especial:
+		relojDisparo.start(elegirIntervaloDisparo())
+		relojDisparo.timeout.connect(func():elegirComportamientoDisparo())
+	else:
+		Disparar()
 func elegirIntervaloDisparo()->float:
 	if dificultad==0:
 		print("Tiempo de disparo:",intervalosDisparo[0])
 		return intervalosDisparo[0]
+	
 	else :
 		var tiempo:float = randf_range(intervalosDisparo[dificultad-1],intervalosDisparo[dificultad])
 		print("Tiempo de disparo:",tiempo)
@@ -39,14 +42,17 @@ func Disparar():
 	print("Orden de disparo")
 	match comportamiento:
 		Comportamiento.rafaga:
-			var bala:RigidBody2D=proyectilNormal.instantiate()
-			
-			espacioBalistico.add_child(bala)
-			bala.position=global_position
-			
-			bala.look_at(jugador.global_position)
-			print("Bala en",bala.position,"mirando a ",bala.rotation_degrees)
-			bala.linear_velocity=Vector2.RIGHT.rotated(bala.rotation)*velocidadesBala[dificultad]
+			for i in range(0,3):
+				var bala:RigidBody2D=proyectilNormal.instantiate()
+				
+				espacioBalistico.add_child(bala)
+				bala.position=global_position
+				
+				bala.look_at(jugador.global_position)
+				print("Bala en",bala.position,"mirando a ",bala.rotation_degrees)
+				bala.linear_velocity=Vector2.RIGHT.rotated(bala.rotation)*(velocidadesBala[dificultad]+0.4*(i+1))
+				await get_tree().create_timer(intervalosDisparo[4]).timeout
+				
 		Comportamiento.circulo:
 			for i in range(0,8):
 				var bala:RigidBody2D=proyectilNormal.instantiate()
@@ -54,8 +60,52 @@ func Disparar():
 				bala.position=global_position
 				bala.rotation_degrees=45*i
 				bala.linear_velocity=Vector2.RIGHT.rotated(bala.rotation)*velocidadesBala[dificultad]
-			
-			
+		
+		Comportamiento.especial:
+					var alerta:RigidBody2D=proyectilEspecial.instantiate()
+					espacioBalistico.add_child(alerta)
+					var posBala1=global_position+Vector2(60,60).rotated(rotation)
+					var rotBala1=45+global_rotation_degrees
+					alerta.position=posBala1
+					alerta.rotation_degrees=rotBala1
+					var alerta2:RigidBody2D=proyectilEspecial.instantiate()
+					espacioBalistico.add_child(alerta2)
+					var posBala2=position+Vector2(-60,60).rotated(rotation)
+					var rotBala2=135+global_rotation_degrees
+					alerta2.position=posBala2
+					alerta2.rotation_degrees=rotBala2
+					var tweenColor = create_tween()
+					tweenColor.set_loops(5)
+					tweenColor.tween_property(alerta,"modulate",Color(1,0,0,1),0.1)
+					tweenColor.tween_property(alerta2,"modulate",Color(1,0,0,1),0.1)
+					await tweenColor.loop_finished
+					alerta.modulate=Color(1,1,1,0.5)
+					alerta2.modulate=Color(1,1,1,0.5)
+					await tweenColor.finished
+					
+					
+					
+					
+					
+					
+					alerta.linear_velocity=Vector2.RIGHT.rotated(alerta.rotation)*2000
+					alerta2.linear_velocity=Vector2.RIGHT.rotated(alerta2.rotation)*2000
+					await get_tree().create_timer(0.25).timeout
+					for i in range(1,4):
+						var bala:RigidBody2D=proyectilEspecial.instantiate()
+						espacioBalistico.add_child(bala)
+						bala.position=posBala1
+						bala.rotation_degrees=rotBala1
+						bala.linear_velocity=Vector2.RIGHT.rotated(bala.rotation)*1200
+						var bala2:RigidBody2D=proyectilEspecial.instantiate()
+						espacioBalistico.add_child(bala2)
+						bala2.position=posBala2
+						bala2.rotation_degrees=rotBala2
+						bala2.linear_velocity=Vector2.RIGHT.rotated(bala2.rotation)*1200
+						await get_tree().create_timer(0.5).timeout
+						look_at(jugador.global_position)
+						rotation_degrees+=-90
+						await Disparar()
 		_:
 			
 			print("Eligiendo otro disparo...")
